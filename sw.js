200f735a247a12391deb6a8d2697f2af2e85cd8b
Rihholf <rihholf@gmail.com>
2026-05-15 14:02:37 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'maui-2026-v6';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.svg',
+  'https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,600;0,700;1,400;1,600&family=Didact+Gothic&family=Tenor+Sans&display=swap',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).then(response => {
+        return caches.open(CACHE).then(cache => {
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      }).catch(() => cached);
+    })
+  );
+});
