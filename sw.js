ac555ffbd60ab7b16d4786fadfd76b1f18ddd278
Rihholf <rihholf@gmail.com>
2026-05-20 19:54:48 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='maui-2026-v9';
+const CACHE='maui-2026-v10';
 const ASSETS=['/maui-2026/','/maui-2026/index.html','/maui-2026/manifest.json','/maui-2026/icon.svg','https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,600;0,700;1,400;1,600&family=Didact+Gothic&family=Tenor+Sans&display=swap','https://unpkg.com/react@18/umd/react.production.min.js','https://unpkg.com/react-dom@18/umd/react-dom.production.min.js','https://unpkg.com/@babel/standalone/babel.min.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
